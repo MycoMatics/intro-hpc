b820233cb048ad93fcdb3ae6ccf478eb8e3b2b03
passelma42 <73598059+passelma42@users.noreply.github.com>
2023-11-14 11:20:21 +0100

    Update hello_world_torque.sh

--- scripts/hello_world_torque.sh
+++ scripts/hello_world_torque.sh
@@ -1,14 +1,16 @@
-#!/usr/bin/bash
-#PBS -N hello-world.$PBS_JOBID
-#PBS -l nodes=1:ppn=1
-#PBS -o stdout.$PBS_JOBNAME.$PBS_JOBID
-#PBS -e stderr.$PBS_JOBNAME.$PBS_JOBID
-#PBS -l walltime=00:02:00
-#PBS -m abe
-#PBS -M pieter.asselman@ugent.be
+#!/usr/bin/bash                                # Line 1 is a she-bang that indicates that this is a Bash script.
 
-module load Python/3.7.2-foss-2018a
+#PBS -N hello-world.$PBS_JOBID                 #  Line 3-7 inform the scheduler about the resources required by this job:
+#PBS -l nodes=1:ppn=1                                      # singe node (nodes=1) single core (ppn=1)
+#PBS -o stdout.$PBS_JOBNAME.$PBS_JOBID                     # redirect sterr stdout to separate files 
+#PBS -e stderr.$PBS_JOBNAME.$PBS_JOBID          
+#PBS -l walltime=00:02:00                                  # run for at most 2 minutes (walltime=00:02:00 max is 72hours)
+#PBS -m abe                                                # send mail when job (a)bort (b)egin (e)nd
+#PBS -M <youremailaddresshere>                             # specify your email address here
 
-cd $PBS_O_WORKDIR
+module load Python/3.7.2-foss-2018a            # load the necessary modules
 
-python hello_world.py
+cd $PBS_O_WORKDIR                              # Line 13: changes the working directory to the directory in which the job will be submitted 
+                                               #(that will be the value of the $PBS_O_WORKDIR environment variable when the job runs).
+
+python hello_world.py                          # Line 16: Does the actual computation
